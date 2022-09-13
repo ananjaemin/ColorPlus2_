f9948493ebbd98875d51d27dc43f405e9264506d
anjaemin <t01089416422@gmail.com>
2022-09-13 13:00:41 +0900

    style: root color 추가,font family 추가

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -6,7 +6,16 @@ export const globalStyle = css`
   ${reset}
 
   :root {
+    --color-backgorund: #0e101f;
+
+    --color-text-primary: #f5f5f5;
+    --color-text-secondary: #906bff;
   }
+  * {
+    box-sizing: border-box;
+    font-family: 'Spoqa Han Sans Neo', sans-serif;
+  }
+
   html {
   }
   body {
