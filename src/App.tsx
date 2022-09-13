c130086180c0bde70f3f341a9ab2fa3a09a63a8e
anjaemin <t01089416422@gmail.com>
2022-09-13 18:24:42 +0900

    feat:Navbar Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 
+import { Navbar } from './components';
+import { MainPage } from './pages';
+
 export const App: React.FC = () => (
   <Routes>
     <Route
       path=""
       element={
         <>
+          <Navbar />
           <Outlet />
         </>
       }
-    ></Route>
+    >
+      <Route index element={<MainPage />} />
+    </Route>
   </Routes>
 );
