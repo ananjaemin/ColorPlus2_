28788cc5f193be389553e8d65972e3e78a1c9e63
anjaemin <t01089416422@gmail.com>
2022-09-14 13:51:44 +0900

    feat: MainLogo

---
+++ src/components/common/MainLogo/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const MainLogo: React.FC = () => <S.MainLogoBackgorund></S.MainLogoBackgorund>;
---
+++ src/components/common/MainLogo/styled.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const MainLogoBackgorund = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  background-color: #ffffff;
+  filter: blur(300px);
+`;
+
+export const MainLogoText = styled.div``;
