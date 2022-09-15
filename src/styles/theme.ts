698abf9f440e317b0a0b4eb0d6d090a0a36b6eca
anjaemin <t01089416422@gmail.com>
2022-09-15 13:55:40 +0900

    feat: theme color

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export const lightTheme = `
+    --color-backgorund: #f3f3f3;
+    --color-logo-background: #424242;
+    --color-background-Line: rgba(0,0,0,0.1);
+    
+    --color-theme-background: #2d2d2d;
+    --color-theme-toggle: #f5f5f5;
+
+    --color-text-primary: #111624;
+    --color-text-secondary: #0a0d15;
+
+    --color-nav-backgorund: #e6e6e6;
+    --color-nav-check-background: #44c577;
+`;
+export const darkTheme = `
+    --color-backgorund: #0a0d15;
+    --color-logo-background: #f5f5f5;
+    --color-background-Line: #000000;
+
+    --color-theme-background: #f5f5f5;
+    --color-theme-toggle: #2d2d2d;
+
+    --color-text-primary: var(--color-logo-background);
+    --color-text-secondary: #7c5cdb;
+
+    --color-nav-backgorund: #2d2d2d;
+`;
