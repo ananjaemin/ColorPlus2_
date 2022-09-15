f527e159320993667d65ea6ab5e0e7af79b116d4
anjaemin <t01089416422@gmail.com>
2022-09-15 13:57:00 +0900

    feat: theme control funtion, ThemeToggleButton 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 
-import { Navbar } from './components';
+import { Navbar, ThemeToggleButton } from './components';
 import { MainPage } from './pages';
 
-export const App: React.FC = () => (
-  <Routes>
-    <Route
-      path=""
-      element={
-        <>
-          <Navbar />
-          <Outlet />
-        </>
-      }
-    >
-      <Route index element={<MainPage />} />
-    </Route>
-  </Routes>
-);
+export const App: React.FC = () => {
+  const [themeName, setThemeName] = useState(true);
+
+  const toggleTheme = async () => {
+    setThemeName(!themeName);
+  };
+
+  useEffect(() => {
+    document.body.dataset.theme = themeName ? 'dark' : 'light';
+  });
+  return (
+    <Routes>
+      <Route
+        path=""
+        element={
+          <>
+            <Navbar />
+            <ThemeToggleButton click={toggleTheme} isTheme={themeName} />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<MainPage />} />
+      </Route>
+    </Routes>
+  );
+};
