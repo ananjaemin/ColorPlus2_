a767418871ae51cc12747a215f3f2cb45d825a7c
anjaemin <t01089416422@gmail.com>
2022-09-15 13:55:05 +0900

    feat: theme setting

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,17 +1,11 @@
 import { css } from '@emotion/react';
 
 import { reset } from './reset';
+import { lightTheme, darkTheme } from './theme';
 
 export const globalStyle = css`
   ${reset}
 
-  :root {
-    --color-backgorund: #0a0d15; // #0e101f
-    --color-background-Line: #333;
-
-    --color-text-primary: #f5f5f5;
-    --color-text-secondary: #906bff;
-  }
   * {
     box-sizing: border-box;
     font-family: 'Spoqa Han Sans Neo', sans-serif;
@@ -20,6 +14,7 @@ export const globalStyle = css`
     font-size: 10px;
   }
   body {
+    ${darkTheme}
     background: var(--color-backgorund);
     color: var(--color-text-primary);
     font-size: 1.6rem;
@@ -28,5 +23,17 @@ export const globalStyle = css`
       linear-gradient(90deg, transparent 49px, var(--color-background-Line) 50px);
     background-size: 50px 50px;
     //https://intrepidgeeks.com/tutorial/is-this-only-css-ok-background-of-checkered-paper-grid
+  }
+  @media (prefers-color-scheme: dark) {
+    body {
+      ${darkTheme}
+    }
+  }
+  body[data-theme='light'] {
+    ${lightTheme};
+  }
+
+  body[data-theme='dark'] {
+    ${darkTheme};
   }
 `;
