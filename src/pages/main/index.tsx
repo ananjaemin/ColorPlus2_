84d3d5e5a548c4bb563b756247b9e76e5daa1f02
anjaemin <t01089416422@gmail.com>
2022-09-22 12:54:07 +0900

    test: RgbRangeSlider Component Test Code 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { MainLogo } from '../../components';
+import { MainLogo, RgbRangeSlider } from '../../components';
+import { getRgbaProps } from '../../utils';
 import * as S from './styled';
 
-export const MainPage: React.FC = () => (
-  <S.MainPageContainer>
-    <MainLogo />
-  </S.MainPageContainer>
-);
+export const MainPage: React.FC = () => {
+  const [redRange, setRedRange] = useState(100);
+  const [greenRange, setGreenRange] = useState(100);
+  const [blueRange, setBlueRange] = useState(100);
+
+  // const [rgbRange, setRgbRange] = useState({ red: 100, green: 100, blue: 100 });
+
+  const {} = getRgbaProps(redRange, greenRange, blueRange, 1);
+  return (
+    <S.MainPageContainer>
+      <MainLogo />
+      <RgbRangeSlider bgColor={`#a35cdb`} min={0} max={255} value={100} setValue={setRedRange} />
+      <RgbRangeSlider
+        bgColor={`rgb(0,255,0)`}
+        min={0}
+        max={255}
+        value={100}
+        setValue={setGreenRange}
+      />
+      <RgbRangeSlider
+        bgColor={`rgb(0,0,255)`}
+        min={0}
+        max={255}
+        value={100}
+        setValue={setBlueRange}
+      />
+    </S.MainPageContainer>
+  );
+};
