2e4f03f60d29c3043ab0393a27dda4c4b386e353
anjaemin <t01089416422@gmail.com>
2022-09-27 13:09:07 +0900

    fix: component name 수정

---
+++ src/components/common/BackLight/styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const MainLogoBackgorund = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+  max-width: 500px;
+  min-width: 300px;
+  height: 500px;
+  border-radius: 50%;
+  background-color: var(--color-logo-background);
+  filter: blur(500px);
+`;
+
+export const MainLogoText = styled.div``;
