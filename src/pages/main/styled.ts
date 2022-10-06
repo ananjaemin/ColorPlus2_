da69454138ab0e49030bd5d33f724f86cb160725
anjaemin <t01089416422@gmail.com>
2022-10-06 22:57:50 +0900

    style: Container

--- src/pages/main/styled.ts
+++ src/pages/main/styled.ts
@@ -1,3 +1,11 @@
 import styled from '@emotion/styled';
 
-export const MainPageContainer = styled.div``;
+export const MainPageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MainTitle = styled.div``;
